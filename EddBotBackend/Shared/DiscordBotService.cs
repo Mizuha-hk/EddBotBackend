7b7941bd7a35f1ce5b7dcaf0426a68f6d094196a
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-09-09 18:46:46 +0900

    プロジェクト ファイルを追加します。

---
+++ EddBotBackend/Shared/DiscordBotService.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace EddBotBackend.Shared
+{
+    public class DiscordBotService : BackgroundService
+    {
+        private DiscordSocketClient _client;
+        private readonly IConfiguration _configuration;
+
+        private string Token => _configuration["DISCORD_BOT_TOKEN"];
+
+        public DiscordBotService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken token)
+        {
+            _client = new DiscordSocketClient(
+                new DiscordSocketConfig
+                {
+                    GatewayIntents = GatewayIntents.Guilds 
+                        | GatewayIntents.GuildMessages 
+                        | GatewayIntents.GuildMessageReactions
+                        | GatewayIntents.MessageContent
+                });
+
+            _client.Log += LogAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+            await _client.LoginAsync(TokenType.Bot, Token);
+            await _client.StartAsync();
+        }
+
+        public override async Task StopAsync(CancellationToken token)
+        {
+            await _client.StopAsync();
+        }
+
+        private Task LogAsync(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }  
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (!(message is SocketUserMessage m) || m.Author.IsBot)
+            {
+                return;
+            }
+
+            await m.Channel.SendMessageAsync($"output : {m.Content}");
+        }
+    }
+}
